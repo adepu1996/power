c94bceb7a9c883a10adf98f577e531e77a85fd4e
Adepu Bharath kumar <Bharath@UNIFIEDIT.INDIA>
2022-01-19 16:16:15 +0530

    power

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <container class="con">
+    <div className="App">
+    
+     <h1>AWS Amplify</h1>
+     <button class="bt">+</button>
+
+    </div>
+    </container>
+  );
+}
+
+export default App;
